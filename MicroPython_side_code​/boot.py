3fdf7fe492059e061d4d0f0416adc580f0028b19
eyhlqaq <mono268@Foxmail.com>
2025-08-08 01:50:36 +0800

    fu

---
+++ MicroPython_side_code​/boot.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append("/py")#防止无法获取到依赖，请根据你的实际情况来判断
+import network
+import time
+from machine import Pin
+def wifi(name="your wifi name", password="your wifi password", timeout=10):
+    """
+    连接Wi-Fi网络1
+    参数:
+        ssid (str): Wi-Fi名称
+        password (str): 密码
+        timeout (int): 超时时间(秒)
+    返回:
+        bool: 是否连接成功
+    """
+    led = Pin(2,Pin.OUT)
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+
+    if not wlan.isconnected():
+        print(f"正在连接 {name}...")
+        wlan.connect(name, password)
+        while not wlan.isconnected() and timeout > 0:
+            time.sleep(1)
+            timeout -= 1
+    if wlan.isconnected():
+        print("连接成功!")
+        print("IP地址:", wlan.ifconfig()[0])
+        led.value(1)
+        time.sleep_ms(500)
+        led.value(0)
+        return True
+    else:
+        print("连接失败")
+        return False
+
+wifi()
+
---
+++ MicroPython_side_code​/main.py
@@ -0,0 +1,101 @@
+from microdot import Microdot
+from machine import Pin,SPI
+import ujson as json
+import os
+import sdcard
+
+def sd():#连接sd卡来的
+    spi = SPI(1, sck=Pin(18), mosi=Pin(23), miso=Pin(19))
+    cs = Pin(5, Pin.OUT)
+    sd1 = sdcard.SDCard(spi, cs)
+    os.mount(sd1, '/sd')
+
+sd()
+app = Microdot()
+list1=[]
+path = "/sd/data"
+
+@app.route("/get" ,methods=['GET'])
+def get(request):#读密码来的
+    def yule():
+        file_path = os.listdir("/sd/data")
+        file_len = len(file_path)
+        if file_len > 0:
+            for i in file_path:
+                return_str = i[:-5]
+                with open(f"{path}/{i}", "r") as f:
+                    file_out = json.load(f)
+                file_out.append(return_str)
+                yield json.dumps(file_out) + "\n"
+        else:
+            yield "null"
+    return yule(), {'Content-Type': 'application/x-ndjson'}
+
+
+@app.route("/GetFile",methods=["GET"])
+def get_file(request):
+    file_path = os.listdir("/sd/data")
+    return  json.dumps(file_path)
+@app.route("/PartGet/<file>",methods=["GET"])
+def part_get(request,file):
+    with open(f"{path}/{file}","r")as f:
+        json_data = json.load(f)
+    return  json.dumps(json_data)
+
+
+@app.route("/post" , methods=['POST'])
+def write(request):#添加密码来的
+    dict1 = request.json#接受客户端的json数据
+    path1 = f"{path}/{dict1[0]}.json"#每个应用或网站单独一个文件（sd卡的优势来的）
+    try:#这行开始
+        os.stat(path1)
+    except OSError:
+        open(path1, 'w').close()#到这行为检查文件是否存在
+    try:#这行开始
+        with open(path1, 'r') as f:
+            json_data = json.load(f)
+    except ValueError:
+        json_data=[]#到这行为检查文件里是否有内容
+    json_data.append(dict1[1])
+    with open(path1, 'w') as f:
+        json.dump(json_data,f)#到这里是添加新内容，而不覆盖原有内容
+
+@app.route("/patch",methods=['PATCH'])
+def patch(request):
+    data = request.json
+    file = data["file"]
+    nums = int(data["nums"])
+    name = data["name"]
+    pwds = data["pwds"]
+    note = data["note"]
+    with open(f"{path}/{file}.json","r")as f:
+        data_json_read = json.load(f)
+    data_json = data_json_read[nums]
+    if name!="":
+        data_json["name"] = name
+    if pwds!="":
+        data_json["pwds"] = pwds
+    if note!="":
+        data_json["note"] = note
+
+    data_json_read[nums]=data_json
+    with open(f"{path}/{file}.json","w") as f:
+        json.dump(data_json_read,f)
+
+
+
+@app.route("/del", methods=["DELETE"])
+def delete(request):
+    data = request.json
+    file = data["file"]
+    pwd_num = data["pwd_num"]
+    print(file)
+    with open(f"{path}/{file}.json","r") as f:
+        json_data = json.load(f)
+    del json_data[int(pwd_num)]
+    with open(f"{path}/{file}.json","w") as f:
+        json.dump(json_data,f)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
+
